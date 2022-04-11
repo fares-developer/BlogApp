071abe26ab5a58eacfe60409f81326d6f1d53d5c
Fares <fendong@estudiante.edib.es>
2022-04-11 19:36:48 +0200

    Introducción a Kotlin: 09 Extensions Functions

--- 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
+++ 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
@@ -2,10 +2,19 @@ package com.example.kotlin
 
 fun main() {
 
-    var input:String
-    do {
-        println("Introduce un numero")
-        input = readLine()!!
-    }while (!input.equals("0"))
+    /*
+    * EXTENTION FUNCTIONS
+    * Una extension function es una funcion que se le aplica a una clase de cierto tipo, transformando
+    * su valor a valor que esta función devuelve como vemos más abajo.
+    * */
+
+    val nombre = "Fares"
+    println(nombre.removeFirstChar())
 
 }
+
+//A continuación creamos nuestro propio extension function, el String hace referencia al tipo de objeto
+// que puede utilizar la función
+
+//this hace referencia al receiver en este caso String
+fun String.removeFirstChar():String = this.substring(1) //Esta es otra forma de hacer return
