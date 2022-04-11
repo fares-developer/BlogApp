fc150a5b5a6da8dea49769d7812d3b004bd95631
Fares <fendong@estudiante.edib.es>
2022-04-11 19:51:16 +0200

    Introducción a Kotlin: 10 Nulabilidad

--- 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
+++ 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
@@ -3,18 +3,26 @@ package com.example.kotlin
 fun main() {
 
     /*
-    * EXTENTION FUNCTIONS
-    * Una extension function es una funcion que se le aplica a una clase de cierto tipo, transformando
-    * su valor a valor que esta función devuelve como vemos más abajo.
+    * NULABILIDAD
+    * En este apartado trabajaremos con los nulos en kotlin. En kotlin el signo ? nos permite
+    * especificar una variable como nula, cuando tengamos que acceder a esta variable hay dos formas
+    * de solucionar la nulabilidad, una es indicando el signo ? antes de llamar a cualquier funcion ó
+    * usar !! el cuál indica explícitamente que la variable no es nula pero es un peligroso, se utiliza
+    * sobre to-do para las pruebas por ejemplo generar un NullPE. En cambio cuando especificamos ?
+    * le decimos al compilador que puede ser null y en caso afirmativo controle la exception, se parece
+    * a un try-catch en java.
+    *
+    * También podemos indicarle que si la variable es nula no ejecute otra instrucción
+    * utilizando el operador Elvis ?:
+    *
+    * Algo muy utilizado en la actualidad para controlar la nulabilidad es usar el operador
+    * "let"
+    *
     * */
 
-    val nombre = "Fares"
-    println(nombre.removeFirstChar())
-
+    val nombre:String? = null
+    // val c = nombre?.length ?: 0//uso de operador elvis
+    nombre?.let {//Sólo se entra aquí cuando la variable no es nula
+        println("La variable nombre no es nula")
+    }
 }
-
-//A continuación creamos nuestro propio extension function, el String hace referencia al tipo de objeto
-// que puede utilizar la función
-
-//this hace referencia al receiver en este caso String
-fun String.removeFirstChar():String = this.substring(1) //Esta es otra forma de hacer return
