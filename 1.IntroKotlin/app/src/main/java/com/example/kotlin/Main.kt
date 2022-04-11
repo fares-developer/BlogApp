faee3fc61954ac846a85a832d04b6113d07793e1
Fares <fendong@estudiante.edib.es>
2022-04-11 20:25:47 +0200

    Introducción a Kotlin: 11 clases genéricas

--- 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
+++ 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
@@ -1,28 +1,25 @@
 package com.example.kotlin
 
 fun main() {
-
     /*
-    * NULABILIDAD
-    * En este apartado trabajaremos con los nulos en kotlin. En kotlin el signo ? nos permite
-    * especificar una variable como nula, cuando tengamos que acceder a esta variable hay dos formas
-    * de solucionar la nulabilidad, una es indicando el signo ? antes de llamar a cualquier funcion ó
-    * usar !! el cuál indica explícitamente que la variable no es nula pero es un peligroso, se utiliza
-    * sobre to-do para las pruebas por ejemplo generar un NullPE. En cambio cuando especificamos ?
-    * le decimos al compilador que puede ser null y en caso afirmativo controle la exception, se parece
-    * a un try-catch en java.
+    * MÉTODOS GENÉRICOS
     *
-    * También podemos indicarle que si la variable es nula no ejecute otra instrucción
-    * utilizando el operador Elvis ?:
+    * Los métodos genéricos reciben muchos tipos de datos distintos como es el caso del
+    * método arrayOf con el cual podemos crear un arrayList de lo que sea, Strings,enteros y String,
+    * objetos de diferente tipo,etc.
     *
-    * Algo muy utilizado en la actualidad para controlar la nulabilidad es usar el operador
-    * "let"
-    *
+    * Para crear una clase o método genérico, debemos indicarlo al crealos,
     * */
 
-    val nombre:String? = null
-    // val c = nombre?.length ?: 0//uso de operador elvis
-    nombre?.let {//Sólo se entra aquí cuando la variable no es nula
-        println("La variable nombre no es nula")
+    var persona = Generic(Persona("Fares",20))
+}
+
+//La F indica cualquier tipo de dato, pero puede ser cualquier letra,generalmente T
+//Podemos pasar más de un valor genérico separando por una coma <F,A>.
+class Generic<F>(valor: F) {
+    init {//inicializamos el constructor
+        println("El valor es $valor")
     }
 }
+
+data class Persona(val nombre: String, val edad: Int)
