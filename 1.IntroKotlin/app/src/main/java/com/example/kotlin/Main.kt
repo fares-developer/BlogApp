ee47bfad301a2f3e7d962fdbb6f0291994e6745c
Fares <fendong@estudiante.edib.es>
2022-04-11 16:57:25 +0200

    Introducción a Kotlin: 05 Constantes

--- 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
+++ 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
@@ -1,25 +1,21 @@
 package com.example.kotlin
+/*
+* CONSTANTES
+* Las constantes en kotlin van precedidas de la palabra clave const, para que las constantes
+* sean accesibles desde cualquier parte de nuestro código es necesario declarar
+* "companion object"
+*
+* */
 
-import android.app.Person
+const val direction = "11 de Abril"
 
 fun main() {
-
-    /*
-    * DATA CLASES
-      Las data clases se utilizan para almacenar algún tipo de información en específico, por ejemplo
-      * un objeto de tipo usuario
-    * */
-
-    val usuario = User("Fares",20)
-    //Con el copy podemos coger los datos de otra clase y modificarlos
-    val usuario2 = usuario.copy(edad = 24)
-    println(usuario.toString())
-    println(usuario2.toString())
-    //Con el operador $ y las llaves podemos hacer operaciones con varibales, llamar funciones,etc.
-    println("Son iguales: ${usuario.equals(usuario2)}")
-
+    println(Constantes.direccion)
 }
 
-//Con el método toString en los data class se imprime to-do su contenido
-data class User(val nombre: String, val edad: Int)
+class Constantes{
+    companion object{// Esto es similar al public static final de java
+        const val direccion = "11 de Abril"
+    }
+}
 
