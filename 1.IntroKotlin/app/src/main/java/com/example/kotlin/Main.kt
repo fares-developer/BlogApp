08d5f9fd113129837308d883dc75524a555ea259
Fares <fendong@estudiante.edib.es>
2022-04-11 19:24:12 +0200

    Introducción a Kotlin: 08 Bucles while y do-while

--- 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
+++ 1.IntroKotlin/app/src/main/java/com/example/kotlin/Main.kt
@@ -2,20 +2,10 @@ package com.example.kotlin
 
 fun main() {
 
-    /*
-    * CONDICIONAL WHEN
-    * El condicional when en kotlin es un equivalente del switch de java, el condicional
-    * when tiene muchas utilidades, para más info míra en la página de kotlin
-    *
-    * */
-
-    when ((1..10).random()) {
-        1 -> println("X vale 1")
-        2 -> println("X vale 2")
-        3 -> println("X vale 3")
-        4 -> println("X vale 4")
-        5 -> println("X vale 5")
-        else -> println("Valor no registrado")
-    }
+    var input:String
+    do {
+        println("Introduce un numero")
+        input = readLine()!!
+    }while (!input.equals("0"))
 
 }
