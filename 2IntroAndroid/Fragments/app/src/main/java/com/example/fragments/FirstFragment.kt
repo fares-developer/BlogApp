4c2e7cb8a35d82b2e7e87f8343312e2c67fbefae
Fares <fendong@estudiante.edib.es>
2022-04-14 18:23:31 +0200

    Introducción a Android: 12 Navegación entre fragments

---
+++ 2IntroAndroid/Fragments/app/src/main/java/com/example/fragments/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.commit
+
+
+//Podemos crear la vista desde el contructor del fragmento así nos ahorramos el onCreateView()
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    //En este método podemos hacer cosas en la vista
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val texto = view.findViewById<TextView>(R.id.texto)
+        val button = view.findViewById<Button>(R.id.navegar)
+        button.setOnClickListener {
+            requireActivity().supportFragmentManager.commit {//Esto es para navegar al otro fragment
+                //Añadir el fragment y lo remplazamos el secondFragment en el contenedor
+                replace(R.id.fragment_container_view,SecondFragment())
+                addToBackStack("primerFragment")//Para poder navegar sin problemas con este id
+            }
+        }
+    }
+}
---
+++ 2IntroAndroid/Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Con esto establecemos el fragmento por defecto, es lo mismo que poner el nombre del fragemnt
+        // en el fragmentContainerView
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)//Para que las animaciones y transiciones se ejecuten correctmn
+            add(R.id.fragment_container_view,FirstFragment())
+        }
+    }
+}
