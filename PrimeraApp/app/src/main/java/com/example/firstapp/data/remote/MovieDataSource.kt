47c6f8d3b6217ca384241e5fd5c33896db75df99
Fares <faresendongpro@gmail.com>
2022-05-02 19:08:43 +0200

    Arquitectura: 55 Concluyendo DataSource

--- PrimeraApp/app/src/main/java/com/example/firstapp/data/remote/MovieDataSource.kt
+++ PrimeraApp/app/src/main/java/com/example/firstapp/data/remote/MovieDataSource.kt
@@ -1,23 +1,16 @@
 package com.example.firstapp.data.remote
 
+import com.example.firstapp.application.AppConstants
 import com.example.firstapp.data.model.Movie
 import com.example.firstapp.data.model.MovieList
+import com.example.firstapp.repository.WebService
 
 //En esta clase incluiremos código que haga llamadas al servidor para traer la info
-class MovieDataSource {
+class MovieDataSource(private val webservice:WebService) {
 
-    fun getUpcomingMovies(): MovieList {
+    suspend fun getUpcomingMovies(): MovieList = webservice.getUpcomingMovies(AppConstants.API_KEY)
 
-        return MovieList()
-    }
+    suspend fun getTopRatedMovies(): MovieList = webservice.getTopRatedMovies(AppConstants.API_KEY)
 
-    fun getTopRatedMovies(): MovieList {
-
-        return MovieList()
-    }
-
-    fun getPopularMovies(): MovieList {
-
-        return MovieList()
-    }
+    suspend fun getPopularMovies(): MovieList = webservice.getPopularMovies(AppConstants.API_KEY)
 }
