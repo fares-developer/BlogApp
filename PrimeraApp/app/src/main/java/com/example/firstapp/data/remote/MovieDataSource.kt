0a9569c067eba4d47a0f1aead76e1ac580b81e3b
Fares <faresendongpro@gmail.com>
2022-05-02 18:16:26 +0200

    Arquitectura: 52 Capa de datos 2/2

--- PrimeraApp/app/src/main/java/com/example/firstapp/data/remote/MovieDataSource.kt
+++ PrimeraApp/app/src/main/java/com/example/firstapp/data/remote/MovieDataSource.kt
@@ -1,5 +1,23 @@
 package com.example.firstapp.data.remote
 
+import com.example.firstapp.data.model.Movie
+import com.example.firstapp.data.model.MovieList
+
 //En esta clase incluiremos código que haga llamadas al servidor para traer la info
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList {
+
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+
+        return MovieList()
+    }
 }
