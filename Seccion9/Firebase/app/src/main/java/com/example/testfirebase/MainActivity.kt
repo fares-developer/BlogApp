74b93f2b26e9a1dbe30f979edc0f16cb4c2fe343
fares-developer <edibfares@gmail.com>
2022-05-24 12:54:02 +0200

    Firebase: 84 Tomando Fotos

--- Seccion9/Firebase/app/src/main/java/com/example/testfirebase/MainActivity.kt
+++ Seccion9/Firebase/app/src/main/java/com/example/testfirebase/MainActivity.kt
@@ -1,49 +1,49 @@
 package com.example.testfirebase
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView:ImageView
+    //private val REQUEST_IMAGE_CAPTURE = 1
+
+    private val responseLauncher = registerForActivityResult(StartActivityForResult()){
+        if (it.resultCode == RESULT_OK) {
+            val imageBitmap = it.data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*
-        * Las llamadas de base de datos son asíncronas y no sabemos cuándo llegarán los datos de internet
-          por eso le vamos a añadir el addOnSuccessLIstener.
-          *
-          Para recibir los datos en tiempo real es necesario el addSnapshotListener
-        * */
 
-        val db = FirebaseFirestore.getInstance()//Obtenemos una instancia de la base de datos
+        imageView = findViewById(R.id.image_view)
+        val bTakePhoto = findViewById<Button>(R.id.btn_take_photo)
 
-        //Consultar información
-        db.collection("ciudades")
-            .document("Palma").addSnapshotListener { value, error ->
-                value?.let {document ->
-                    val ciudad = document.toObject(Ciudad::class.java)
+        bTakePhoto.setOnClickListener { dispatchTakePictureIntente() }
+    }
 
-                    Log.i("Firebase", "${ciudad?.population}")
-                    Log.i("Firebase", "${ciudad?.clima}")
-                    Log.i("Firebase", "${ciudad?.continente}")
-                }
-            }
-
-
-        //Ingresar informacion
-        //Se puede ingresar cualquier tipo de información
-        db.collection("ciudades")
-            .document("Siberia")
-            .set(Ciudad(100, "invierno", "Asia"))
-            .addOnSuccessListener {
-                Log.i("Firebase", "Se han agregado los datos")
-            }.addOnFailureListener {
-                Log.e("FirebaseError", it.toString())
-            }
-
+    //En este método generamos un intent explícito que inicie la cámara
+    private fun dispatchTakePictureIntente() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            //startActivityForResult(intent,REQUEST_IMAGE_CAPTURE)
+            responseLauncher.launch(intent)
+        }catch (e: ActivityNotFoundException){
+            Toast.makeText(this,"No se encontró una app para la foto",Toast.LENGTH_SHORT).show()
+        }
     }
 }
-
-//Las variables tienen que tener un valor por defecto para que en firebase no sea null
-data class Ciudad(val population: Int = 0, val clima: String = "", val continente: String = "")
