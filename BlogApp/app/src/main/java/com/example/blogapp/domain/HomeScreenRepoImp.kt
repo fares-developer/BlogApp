bc4cc8aa723f24e36dc8a0642a9834bb35cea5b5
fares-developer <edibfares@gmail.com>
2022-05-25 23:38:00 +0200

    BlogApp: 97 capa de datos

--- BlogApp/app/src/main/java/com/example/blogapp/domain/HomeScreenRepoImp.kt
+++ BlogApp/app/src/main/java/com/example/blogapp/domain/HomeScreenRepoImp.kt
@@ -2,11 +2,10 @@ package com.example.blogapp.domain
 
 import com.example.blogapp.core.Resource
 import com.example.blogapp.data.model.Post
+import com.example.blogapp.data.remote.HomeScreenDataSource
 
 //Esta clase implementa la interfaz HomeScreenRepo
-class HomeScreenRepoImp(private val dataSource:HomeScreenDataSource):HomeScreenRepo {
+class HomeScreenRepoImp(private val dataSource: HomeScreenDataSource):HomeScreenRepo {
 
-    override suspend fun getLatestPosts(): Resource<List<Post>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getLatestPosts(): Resource<List<Post>> = dataSource.getLatestPosts()
 }
