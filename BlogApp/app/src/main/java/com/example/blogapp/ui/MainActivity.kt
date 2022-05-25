a68045f486276bf2971de6b693518d61280707c0
fares-developer <edibfares@gmail.com>
2022-05-25 13:21:42 +0200

    BlogApp: 93 Creando nuestra UI

--- BlogApp/app/src/main/java/com/example/blogapp/ui/MainActivity.kt
+++ BlogApp/app/src/main/java/com/example/blogapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.blogapp
+package com.example.blogapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.blogapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ BlogApp/app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
@@ -0,0 +1,12 @@
+package com.example.blogapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.blogapp.R
+
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+
+}
