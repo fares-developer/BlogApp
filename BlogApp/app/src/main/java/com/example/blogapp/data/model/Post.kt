34806dad11e1464420ed8f426fcf71b88f7dafaf
fares-developer <edibfares@gmail.com>
2022-05-25 11:29:27 +0200

    BlogApp: 92 Creando nuestro modelo

---
+++ BlogApp/app/src/main/java/com/example/blogapp/data/model/Post.kt
@@ -0,0 +1,10 @@
+package com.example.blogapp.data.model
+
+import com.google.firebase.Timestamp
+
+data class Post(
+    val profile_picture: String = "",//Imagen de perfil
+    val profile_name: String = "",
+    val post_timestamp: Timestamp? = null,//Por ejemplo hace 2 horas
+    val post_image: String = ""//Imagen que mostraremos al usuario
+)
