435eefee7cfd512d293618bead46914cf4311a1f
fares-developer <edibfares@gmail.com>
2022-05-26 15:28:39 +0200

    BlogApp: 105 Validando datos del registro

---
+++ BlogApp/app/src/main/java/com/example/blogapp/presentation/auth/RegisterScreenViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.blogapp.presentation.auth
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.blogapp.core.Resource
+import kotlinx.coroutines.Dispatchers
+
+class RegisterScreenViewModel {
+
+}
--- BlogApp/app/src/main/java/com/example/blogapp/ui/auth/RegisterFragment.kt
+++ BlogApp/app/src/main/java/com/example/blogapp/ui/auth/RegisterFragment.kt
@@ -1,16 +1,61 @@
 package com.example.blogapp.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.blogapp.R
+import com.example.blogapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var binding: FragmentRegisterBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding = FragmentRegisterBinding.bind(view)
+        signUp()
+
+    }
+
+    private fun signUp() {
+
+        binding.btnSignup.setOnClickListener {
+
+            val username = binding.editTextUsername.text.toString().trim()
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+            if (password != confirmPassword) {
+                binding.editTextConfirmPassword.error = "Password does not match"
+                binding.editTextPassword.error = "Password does not match"
+
+                //Este tipo de return es para que cuando aparezca el error no continue la ejecución
+                return@setOnClickListener
+            }
+
+            if (username.isEmpty()) {
+                binding.editTextUsername.error = "User name is empty"
+                return@setOnClickListener
+            }
+
+            if (email.isEmpty()) {
+                binding.editTextEmail.error = "E-mail is empty"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                binding.editTextPassword.error = "Password is empty"
+                return@setOnClickListener
+            }
+
+            if (confirmPassword.isEmpty()) {
+                binding.editTextUsername.error = "Confirm is empty"
+                return@setOnClickListener
+            }
+
+
+        }
     }
 }
