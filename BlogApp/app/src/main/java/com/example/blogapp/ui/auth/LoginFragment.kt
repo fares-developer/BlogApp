80cfdbe58de61904e2fff419d85b29152a25dc47
fares-developer <edibfares@gmail.com>
2022-05-26 11:23:21 +0200

    BlogApp: 101 LoginFragment UI 2

--- BlogApp/app/src/main/java/com/example/blogapp/ui/auth/LoginFragment.kt
+++ BlogApp/app/src/main/java/com/example/blogapp/ui/auth/LoginFragment.kt
@@ -1,21 +1,59 @@
 package com.example.blogapp.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.blogapp.R
 import com.example.blogapp.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
 
+    //by Lazy lo que hace es inicializar la variable cuando se utiliza
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentLoginBinding.bind(view)
+        isUserLoggedIn()
+        doLogin()
     }
+
+    private fun isUserLoggedIn() {
+        firebaseAuth.currentUser?.let {
+            findNavController().navigate(R.id.action_loginFragment_to_homeScreenFragment)
+        }
+    }
+
+    private fun doLogin() {
+        binding.btnSignin.setOnClickListener {
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            validateCredentials(email,password)
+            signIn(email,password)
+
+        }
+    }
+
+    private fun validateCredentials(email: String, password: String) {
+        if (email.isEmpty()) {
+            binding.editTextEmail.error = "E-mail is empty"
+            return
+        }
+
+        if (password.isEmpty()) {
+            binding.editTextPassword.error = "Password is empty"
+            return
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+
+    }
+
 }
