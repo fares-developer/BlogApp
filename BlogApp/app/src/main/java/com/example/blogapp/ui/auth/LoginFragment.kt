53dc95684eebf1d700a5012cdad7061048a833dc
fares-developer <edibfares@gmail.com>
2022-05-27 17:00:24 +0200

    BlogApp: 113 Fixeando bug popup a login desde setupProfile

--- BlogApp/app/src/main/java/com/example/blogapp/ui/auth/LoginFragment.kt
+++ BlogApp/app/src/main/java/com/example/blogapp/ui/auth/LoginFragment.kt
@@ -45,8 +45,13 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     }
 
     private fun isUserLoggedIn() {
+
         firebaseAuth.currentUser?.let {
-            findNavController().navigate(R.id.action_loginFragment_to_homeScreenFragment)
+            if (it.displayName.toString().isNullOrEmpty()) {
+                findNavController().navigate(R.id.action_loginFragment_to_setupProfileFragment)
+            } else {
+                findNavController().navigate(R.id.action_loginFragment_to_homeScreenFragment)
+            }
         }
     }
 
@@ -82,7 +87,11 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
                 is Result.Success -> {
                     binding.progressBar.hide()
-                    findNavController().navigate(R.id.action_loginFragment_to_homeScreenFragment)
+                    if (it.data?.displayName.isNullOrEmpty()) {
+                        findNavController().navigate(R.id.action_loginFragment_to_setupProfileFragment)
+                    } else {
+                        findNavController().navigate(R.id.action_loginFragment_to_homeScreenFragment)
+                    }
                 }
 
                 is Result.Failure -> {
