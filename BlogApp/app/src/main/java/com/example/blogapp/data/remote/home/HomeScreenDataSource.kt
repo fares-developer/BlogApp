1d96ee3ecfa72b901405653715ffc58e65efcefd
fares-developer <edibfares@gmail.com>
2022-05-28 20:54:48 +0200

    BlogApp: 119 Ordenar posts por fecha de creación

--- BlogApp/app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
+++ BlogApp/app/src/main/java/com/example/blogapp/data/remote/home/HomeScreenDataSource.kt
@@ -4,6 +4,7 @@ import com.example.blogapp.core.Result
 import com.example.blogapp.data.model.Post
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.tasks.await
 
 //En esta clase estará el método que irá a buscar la info a Firebase
@@ -11,7 +12,8 @@ class HomeScreenDataSource {
 
     suspend fun getLatestPosts(): Result<List<Post>> {
         val postList = mutableListOf<Post>()
-        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").get().await()
+        val querySnapshot = FirebaseFirestore.getInstance()
+            .collection("posts").orderBy("created_at",Query.Direction.DESCENDING).get().await()
 
         for (post in querySnapshot.documents) {
             //Transformamos el snapshot post a un Post objeto y lo añadimos a la lista de Post
