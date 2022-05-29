796ded01e0eb26f39a01c0c1fa798495f6586492
fares-developer <edibfares@gmail.com>
2022-05-29 22:22:24 +0200

    BlogApp: 122 Creando likes en nuestro post

--- BlogApp/app/src/main/java/com/example/blogapp/data/model/Post.kt
+++ BlogApp/app/src/main/java/com/example/blogapp/data/model/Post.kt
@@ -1,14 +1,25 @@
 package com.example.blogapp.data.model
 
+import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.*
 
 data class Post(
-    val profile_picture: String = "",//Imagen de perfil
-    val profile_name: String = "",
+    @Exclude @JvmField
+    val id: String = "",
     @ServerTimestamp //Esto hace que create_at se cree con tiempo del servidor si lo mandamos null
     var create_at: Date? = null,//Por ejemplo hace 2 horas
     val post_image: String = "",//Imagen que mostraremos al usuario
     val post_description: String = "",
-    val uid:String=""
+    val poster: Poster? ,
+    val likes: Long = 0,
+    @Exclude @JvmField //Esto evita que la aplicación se rompa ya que liked no existe en firebase
+    val liked: Boolean = false
+)
+
+//Persona que sube un post
+data class Poster(
+    val username: String? = "",
+    val uid: String? = null,
+    val profile_picture: String = ""
 )
